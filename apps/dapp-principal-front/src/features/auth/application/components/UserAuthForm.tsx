49f88292194a42962f4b1a3b45cd8d245f0f101e
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-11-02 13:09:30 -0500

    feat (principal-front): agrgea el formulario de auth

---
+++ apps/dapp-principal-front/src/features/auth/application/components/UserAuthForm.tsx
@@ -0,0 +1,98 @@
+import { Button } from "@/shared/application/components/custom/Button";
+import { PasswordInput } from "@/shared/application/components/custom/PasswordInput";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shared/application/components/ui/form";
+import { Input } from "@/shared/application/components/ui/input";
+import { cn } from "@/shared/application/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { HTMLAttributes, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+interface UserAuthFormProps extends HTMLAttributes<HTMLDivElement> {
+  nonImplemented?: boolean;
+}
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Por favor, ingresa tú email" })
+    .email({ message: "Email Invalido" }),
+  password: z
+    .string()
+    .min(1, {
+      message: "Por favor, ingresa tú contraseña",
+    })
+    .min(7, {
+      message: "La contraseña debe tener al menos 7 caracteres",
+    }),
+});
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    setIsLoading(true);
+    console.log(data);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="space-y-1">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="name@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="space-y-1">
+                  <div className="flex items-center justify-between">
+                    <FormLabel>Password</FormLabel>
+                  </div>
+                  <FormControl>
+                    <PasswordInput placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="mt-2" loading={isLoading}>
+              Login
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
