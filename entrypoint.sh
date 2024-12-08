013b7a593ae15601a0a7a24712ced53e261febe7
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-12-08 03:27:33 -0500

    refactor: add queue data

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,27 +4,27 @@
 NETWORK_NAME="dapp_network"
 
 echo ">>>>>>> Deteniendo y eliminando contenedores, imágenes y volúmenes..."
-docker-compose down --rmi all --volumes --remove-orphans
+sudo docker compose down --rmi all --volumes --remove-orphans
 
 echo ">>>>>>> Verificando red $NETWORK_NAME..."
-docker network inspect $NETWORK_NAME >/dev/null 2>&1
+sudo docker network inspect $NETWORK_NAME >/dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo ">>>>>>> Red $NETWORK_NAME no encontrada. Creando..."
-  docker network create --driver bridge $NETWORK_NAME
+  sudo docker network create --driver bridge $NETWORK_NAME
 else
   echo ">>>>>>> Red $NETWORK_NAME ya existe."
 fi
 
 echo ">>>>>>> Levantando contenedores..."
-docker-compose up -d
+sudo docker compose up -d
 
 echo ">>>>>>> Conectando contenedores a la red $NETWORK_NAME..."
 
 for container in $(docker ps --format "{{.Names}}"); do
-  docker network connect $NETWORK_NAME $container 2>/dev/null || true
+  sudo docker network connect $NETWORK_NAME $container 2>/dev/null || true
 done
 
 echo ">>>>>>> Verificando conexiones a la red $NETWORK_NAME..."
-docker network inspect $NETWORK_NAME
+sudo docker network inspect $NETWORK_NAME
 
 echo ">>>>>>> Todo listo. ¡Sistema en ejecución!"
