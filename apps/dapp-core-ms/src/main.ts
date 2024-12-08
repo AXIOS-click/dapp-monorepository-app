fc32c5cf2b8e9cb74d9714b63ca3c5cb602d1e4e
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-12-07 20:43:00 -0500

    refactor: error in stack trace based in golang

--- apps/dapp-core-ms/src/main.ts
+++ apps/dapp-core-ms/src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import tracer from './batch-telemetry';
 
 async function bootstrap() {
-  await tracer.start();
+  // await tracer.start();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3001, '0.0.0.0');
